aa0616432ec2ae8a4d639396b58bbe47105070f0
nhuydev1704 <khanhandli@gmail.com>
2022-10-11 14:05:38 +0700

    [Feature]: setup cicd

--- src/features/App/customer/pages/index.tsx
+++ src/features/App/customer/pages/index.tsx
@@ -2,7 +2,7 @@ import PageLayout from '@/layout';
 import React from 'react';
 
 const CustomerPage = () => {
-    return <div>{hihi.anem}</div>;
+    return <div>123</div>;
 };
 
 export default CustomerPage;
