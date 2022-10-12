b9a4f42a491331ae2e7052c1a8de0b94a103cd60
nguyenduy0608 <Nguyendduy0604@gmail.com>
2022-10-12 11:42:32 +0700

    [Feature]: voucher

--- src/features/App/product/page/Category.tsx
+++ src/features/App/product/page/Category.tsx
@@ -2,25 +2,43 @@ import CardComponent from '@/components/CardComponent';
 import TableComponent from '@/components/TableComponent';
 import TopBar from '@/components/TopBar';
 import { PADDING } from '@/config/theme';
-import { Card, Descriptions, Segmented } from 'antd';
-import React from 'react';
+import { Button, Card, Descriptions, Segmented, Switch } from 'antd';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { EditOutlined, DeleteOutlined, SettingOutlined } from '@ant-design/icons';
 import { dataSourceCategoryHaNoi, columsCategoryHanoi } from '../components/Product.Config';
 
 const ProductCategoryPage = () => {
     const [count, setCount] = React.useState(0);
+    const [loading, setLoading] = useState(true);
 
+    const onChange = (checked: boolean) => {
+        setLoading(!checked);
+    };
     return (
         <Container>
-            <TopBar title="Danh mục sản phẩm" />
+            <TopBar
+                title="Danh mục sản phẩm"
+                extra={
+                    <Button className="gx-mb-0" type="primary">
+                        Tìm kiếm
+                    </Button>
+                }
+            />
             <CardComponent title={<Segmented options={['Hà Nội', 'Vinh', 'Hồ Chí Minh']} />} extra={<div>filter</div>}>
                 <TableComponent
                     page={1}
                     rowSelect={false}
                     onChangePage={(_page) => console.log(_page)}
                     expandedRowRender={(row: any) => (
-                        <Card className="gx-mb-0">
-                            <Descriptions title="Thông tin danh mục">
+                        <Card
+                            className="gx-mb-0"
+                            actions={[
+                                <EditOutlined key="edit">Chỉnh sửa</EditOutlined>,
+                                <Switch checked={!loading} onChange={onChange} />,
+                            ]}
+                        >
+                            <Descriptions title="Thông tin danh mục" column={2}>
                                 <Descriptions.Item label="Tên danh mục">
                                     {dataSourceCategoryHaNoi[0].name}
                                 </Descriptions.Item>
--- src/features/App/voucher/page/index.tsx
+++ src/features/App/voucher/page/index.tsx
@@ -1,7 +1,82 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import TableComponent from '@/components/TableComponent';
+import { Button, Card, DatePicker, Descriptions, Input, Segmented, Switch } from 'antd';
+import { dataSourceVoucher, columnsVoucher } from '../components/Voucher.Config';
+import { EditOutlined, DeleteOutlined, SettingOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+import TopBar from '@/components/TopBar';
+import CardComponent from '@/components/CardComponent';
+import { PADDING } from '@/config/theme';
+import moment from 'moment';
 const VoucherPage = () => {
-    return <div>VoucherPage</div>;
+    const [loading, setLoading] = useState(true);
+    const { RangePicker } = DatePicker;
+    const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY'];
+    const dateFormat = 'DD/MM/YYYY';
+    const onChange = (checked: boolean) => {
+        setLoading(!checked);
+    };
+    return (
+        <Container>
+            <TopBar
+                title="Quản lý voucher"
+                extra={
+                    <Button className="gx-mb-0" type="primary">
+                        Thêm mới
+                    </Button>
+                }
+            />
+            <CardComponent
+                title=""
+                extra={[
+                    <RangePicker
+                        defaultValue={[moment('2015/01/01', dateFormat), moment('2015/01/01', dateFormat)]}
+                        format={dateFormat}
+                    />,
+                ]}
+            >
+                <TableComponent
+                    page={1}
+                    rowSelect={false}
+                    onChangePage={(_page) => console.log(_page)}
+                    expandedRowRender={(row: any) => (
+                        <Card
+                            className="gx-mb-0"
+                            actions={[
+                                <div>
+                                    <Switch checked={!loading} onChange={onChange} />
+                                    Đang hoạt động
+                                </div>,
+                                <div style={{ marginTop: '12px' }}>
+                                    <EditOutlined key="edit" />
+                                    Chỉnh sửa
+                                </div>,
+                                <div>
+                                    <DeleteOutlined key="delete" />
+                                    Xóa
+                                </div>,
+                            ]}
+                        >
+                            <Descriptions title="Thông tin mã giảm giá" column={2}>
+                                <Descriptions.Item label="Mã voucher">KM001</Descriptions.Item>
+                                <Descriptions.Item label="Ngày bắt đầu">19/07/2020</Descriptions.Item>
+                                <Descriptions.Item label="Tên voucher">Mua hóa đơn 10tr giảm 2%</Descriptions.Item>
+                                <Descriptions.Item label="Ngày kết thúc">20/08/2010</Descriptions.Item>
+                                <Descriptions.Item label="Số lượng quy định">20</Descriptions.Item>
+                                <Descriptions.Item label="Số lượng còn lại">10</Descriptions.Item>
+                            </Descriptions>
+                        </Card>
+                    )}
+                    columns={columnsVoucher}
+                    dataSource={dataSourceVoucher}
+                    total={dataSourceVoucher.length}
+                />
+            </CardComponent>
+        </Container>
+    );
 };
 
+const Container = styled.div`
+    padding: ${PADDING};
+`;
 export default VoucherPage;
