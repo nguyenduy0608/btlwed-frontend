7dfea00ede26b74293577ab4803fa15e7c937708
nhuydev1704 <khanhandli@gmail.com>
2022-11-29 11:03:07 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 export default defineConfig({
     resolve: {
         alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+        mainFields: [],
     },
     plugins: [react()],
     build: {
