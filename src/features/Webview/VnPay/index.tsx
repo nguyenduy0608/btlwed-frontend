29ffd0f8ab6240a407735da11443b59e7bf9b075
nhuydev1704 <khanhandli@gmail.com>
2023-01-16 15:41:50 +0700

    Update index.tsx

--- src/features/Webview/VnPay/index.tsx
+++ src/features/Webview/VnPay/index.tsx
@@ -2,23 +2,48 @@ import React from 'react';
 import styled from 'styled-components';
 import { images } from '@/assets/imagesAssets';
 import { Button, Row } from 'antd';
+import { currencyFormat } from '@/utils';
+import TagResult from '@/components/TagResult';
 
 const VnPayPage = () => {
+    const params = new URLSearchParams(window.location.search);
+    const obj = Object.fromEntries(params);
+    const { vnp_TxnRef } = obj;
     return (
         <Container>
             <div>
-                <Row justify="center">
+                <Row justify="center" className="gx-mb-4">
                     <img src={images.logo} alt="logo_app" />
                 </Row>
-                <h2>Thanh toán thành công</h2>
+                <Row justify="center">
+                    {obj?.vnp_ResponseCode !== '00' ? (
+                        <TagResult
+                            style={{ fontSize: '14px', padding: '8px 20px' }}
+                            color="error"
+                            text="Thanh toán thất bại"
+                        />
+                    ) : (
+                        <TagResult
+                            style={{ fontSize: '14px', padding: '8px 20px' }}
+                            color="success"
+                            text="Thanh toán thành công"
+                        />
+                    )}
+                </Row>
+                <Row justify="center" className="gx-mt-4">
+                    <span className="gx-mr-2">Số tiền: </span>
+                    <strong>{currencyFormat(+obj?.vnp_Amount)}</strong>
+                </Row>
                 <Row justify="center" className="gx-mt-5">
                     <Button
                         onClick={() => {
-                            window.location.href = `staka://success?order_id=${1}`;
+                            obj?.vnp_ResponseCode !== '00'
+                                ? (window.location.href = 'staka://failed/')
+                                : (window.location.href = `staka://success?order_id=${vnp_TxnRef?.split('_')?.[0]}`);
                         }}
                         type="primary"
                     >
-                        Tiếp tục
+                        Quay lại ứng dụng
                     </Button>
                 </Row>
             </div>
